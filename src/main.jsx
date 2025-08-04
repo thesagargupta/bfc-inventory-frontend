91cd6403486d042eb91813b9c01c1ac6022f88b3
sagar gupta <sagarkshn8@gmail.com>
2025-08-04 13:46:17 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "../src/components/Home";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 1. Create a new instance of QueryClient
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* 2. Wrap your application with the QueryClientProvider and pass the client */}
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
